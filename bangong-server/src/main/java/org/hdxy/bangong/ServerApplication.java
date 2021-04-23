5e30df6ca52a2c4d831c633344b7d0cffe7d1932
SkyGlad <1109259152@qq.com>
2021-04-23 10:33:51 +0800

    登录完成

--- bangong-server/src/main/java/org/hdxy/bangong/ServerApplication.java
+++ bangong-server/src/main/java/org/hdxy/bangong/ServerApplication.java
@@ -5,11 +5,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
+/**
+ * 启动类
+ */
 @SpringBootApplication
 @EnableWebSecurity
 @MapperScan("org.hdxy.bangong.mapper")
 public class ServerApplication {
     public static void main(String[] args) {
-        SpringApplication.run(ServerApplication.class,args);
+        SpringApplication.run(ServerApplication.class, args);
     }
 }
